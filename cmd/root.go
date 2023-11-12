9ec9cfe9f967eb24b14590c2b5d621ffd0791d33
AleksanderWWW <alwojnarowicz@gmail.com>
2023-11-12 23:07:20 +0100

    add pre-commit and gh actions

--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,8 +20,6 @@ import (
 
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -61,5 +59,3 @@ func init() {
 	rootCmd.PersistentFlags().String("end-date", "", "sample usage")
 	rootCmd.PersistentFlags().String("out", "stdout", "sample usage")
 }
-
-
