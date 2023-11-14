b2271a38cbcc161a3e33d9b0cf746bea4b633c2b
AleksanderWWW <alwojnarowicz@gmail.com>
2023-11-14 19:48:45 +0100

    more tests

--- cmd/utils_test.go
+++ cmd/utils_test.go
@@ -1,6 +1,11 @@
 package cmd
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/spf13/cobra"
+)
 
 func TestIsCSV(t *testing.T) {
 	type test struct {
@@ -39,6 +44,69 @@ func TestIsCSV(t *testing.T) {
 		out := isCSV(testItem.in)
 		if out != testItem.expectedOut {
 			t.Errorf("Failed checking if CSV:\n In: %s\n Expected: %t\n Actual: %t", testItem.in, testItem.expectedOut, out)
+		}
+	}
+}
+
+func TestGetWriterFunc(t *testing.T) {
+
+	goodOuts := []string{
+		"stdout",
+		"file.csv",
+	}
+
+	badOuts := []string{
+		"abc",
+		"csv",
+		"json",
+		"xlsx",
+	}
+
+	for _, goodOut := range goodOuts {
+		_, err := getWriterFunc(goodOut)
+		if err != nil {
+			t.Errorf("Unexpected error for %s - %s", goodOut, err)
+		}
+	}
+
+	for _, badOut := range badOuts {
+		_, err := getWriterFunc(badOut)
+		if err == nil {
+			t.Errorf("Expected error for %s, but did not get it", badOut)
+		}
+	}
+}
+
+func TestParseDate(t *testing.T) {
+	type test struct {
+		in          string
+		expectedOut time.Time
+	}
+
+	tests := []test{
+		{
+			in:          "",
+			expectedOut: time.Time{},
+		},
+		{
+			in:          "2023-12-31",
+			expectedOut: time.Date(2023, 12, 31, 0, 0, 0, 0, time.UTC),
+		},
+	}
+
+	for _, test := range tests {
+		out, err := parseDate(test.in)
+
+		if err != nil {
+			t.Errorf("Unexpected error while parsing dates - %s", err)
+		}
+
+		if out != test.expectedOut {
+			t.Errorf(
+				"Wrong output parseDate:\n Expected: %s\n Actual: %s",
+				test.expectedOut,
+				out,
+			)
 		}
 	}
 }
